87c55b8d9456191ec7e78bf78bad3919996aa80b
Steve Simonson <srsimonson@gmail.com>
2021-01-08 22:50:31 -0600

    added apple

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import apple from './apple.png';
 import banana from './banana.png';
-import leaf from './icon.png';
 import styles from './index.module.scss';
 
 
@@ -9,19 +9,15 @@ const Navigation = () => {
     return (
         <div>
             <ul>
-                <li> <button>1</button> Find Orgs</li>
-                <li> <button>2</button> Save Energy</li>
-                <li> <button>3</button> Shared Solar</li>
-                <li> <button>4</button> Take Action</li>
-                <li> <button>5</button> Rooftop Solar</li>
-                <li> <button>6</button> Write Your Rep</li>
-                <li> <button>7</button> About</li>
                 <li>
-                    <button 
-                        className={styles.banana}> 
-                        <img className={styles.banana} src={banana}/> 
-                    </button>
-                    banana
+                <button className={styles.banana}><img className={styles.banana} src={apple}/></button>
+                    <span className={styles.hidden}>APPLE</span>
+                </li>
+                
+              {/* ///////////////////////// */}
+                <li>
+                    <button className={styles.banana}><img className={styles.banana} src={banana}/></button>
+                    <span className={styles.hidden}>BANANA</span>
                 </li>
             </ul>
         </div>
